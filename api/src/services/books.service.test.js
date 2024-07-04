5956990a524c3e74197929261ec25f816091ddd7
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-03 20:35:35 -0500

    Add Mocking usage

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,10 +1,32 @@
 const BooksService = require('./books.service');
 
+// se genera fake data para simular la respuesta de la BD
+const fakeBooks = [
+  {
+    _id: '60f7b3b3b3b3b3b3b3b3b3b3',
+    name: 'Harry Potter',
+  },
+  {
+    _id: '60f7b3b3b3b3b3b3b3b3b3',
+    name: 'The Witcher',
+  }
+];
+
+// se crea un stub para simular el comportamiento de la BD
+const MongoLibStub = {
+  getAll: async () => [...fakeBooks],
+  create: async () => { }
+}
+
+// cuando BooksService se importe, se va a reemplazar el constructor de MongoLib por MongoLibStub y los métodos que seran usados son los de MongoLibStub
+jest.mock('../lib/mongo.lib.js', () => jest.fn().mockImplementation(() => MongoLibStub)); // se pasa el path del archivo a mockear y una función que retorna el stub
+
 describe('Test for BooksService', () => {
   let service;
 
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks(); // limpia los mocks antes de cada test de forma que no se mezclen los mocks de un test con otro y se mantenga aislado
   });
 
   describe('Test for getBooks', () => {
